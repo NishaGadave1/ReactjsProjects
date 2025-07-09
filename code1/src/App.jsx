fb83b4aef76dbe2e6bb9a967e6616c3e3960575c
nishu <nishagadave@gym.com>
2025-07-09 17:29:05 +0530

    made changes in Ludogame

--- code1/src/App.jsx
+++ code1/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
 import Ludogame from './Ludogame'
+// import Counter from './Counter'
+
 function App() {
 
   return (
     <>
       <Ludogame/>
+      {/* <Counter/> */}
     </>
   )
 }
